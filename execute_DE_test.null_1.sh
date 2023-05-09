947a271715cbf53727cd8b817a0ee012c16f3b1b
mconery <mitchell.l.conery@gmail.com>
2023-05-09 16:41:19 -0400

    Initial commit

---
+++ execute_DE_test.null_1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -o execute_DE_test.null_1.log
+#SBATCH --time=7-00:00:00
+#SBATCH --cpus-per-task 60
+#SBATCH --mem=180G
+
+################################### Set User Variables ###############################
+
+de_test_script=/mnt/isilon/sfgi/conerym/analyses/grant/crispri_screen/HFOB_screen_bone/Grant_hFOB_CRISPRi/test_differential_expression.R
+test_type=in_out
+cell_type=single_guide
+
+######################################################################################
+
+#Activate modules
+module load R/4.2.3
+
+Rscript $de_test_script $test_type $cell_type
+
