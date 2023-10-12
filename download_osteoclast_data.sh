cf150d9e44627fa48707fe48ee7b2c5921a23866
mconery <mitchell.l.conery@gmail.com>
2023-10-12 01:45:58 -0400

    Initial Commit

---
+++ download_osteoclast_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -o download_osteoclast_data.log
+#SBATCH --time=2-00:00:00
+#SBATCH --mem=4G
+
+################################### Set Files ########################################
+
+chip_dir=/mnt/isilon/sfgi/conerym/data/ChIP-seq/osteoclasts
+atac_dir=/mnt/isilon/sfgi/conerym/data/ATAC-seq/osteoclasts
+
+bae_atac_acc_file=$atac_dir/Bae_2022.ATAC.SRR_Acc_List.txt
+bae_chip_acc_file=$chip_dir/Bae_2022.H3K27ac.SRR_Acc_List.txt
+tesfaye_chip_acc_file=$chip_dir/Tesfaye_2023.H3K27ac.SRR_Acc_List.txt
+
+######################################################################################
+
+for file in $bae_atac_acc_file $bae_chip_acc_file $tesfaye_chip_acc_file; do 
+	out_dir=$(dirname $file)
+	while read accession; do 
+		fastq-dump $accession -O $out_dir
+	done < $file
+done
