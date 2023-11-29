e0215c705d5976279e6551fb82b5cb8c2f76b128
mconery <mitchell.l.conery@gmail.com>
2023-11-29 09:08:21 -0500

    Fixed bug with not calling full pkl file location

--- Multi-Trait_Fine-Mapping/3_fine-mapping/plot_all_results.sh
+++ Multi-Trait_Fine-Mapping/3_fine-mapping/plot_all_results.sh
@@ -18,10 +18,13 @@ temp_script_dir=$finemap_dir/temp_script_files
 
 ###########################################################################################################################################################
 
+#Switch folder for temp file generation
+cd $temp_script_dir
+
 #Loop over the loci and plot each
 for file in $(ls $cafeh_dir); do 
 file_prefix=${file/%.pkl/}
-echo "source activate cafeh; python $plot_script -p $file -o $plot_dir -t non-residual -u 0.1" > $temp_script_dir/$file_prefix.plot.sh
+echo "source activate cafeh; python $plot_script -p $cafeh_dir/$file -o $plot_dir -t non-residual -u 0.1" > $temp_script_dir/$file_prefix.plot.sh
 sed -i '1i#!/bin/bash' $temp_script_dir/$file_prefix.plot.sh
 sbatch --mem=4G -t 2:00:00 --job-name $file_prefix $temp_script_dir/$file_prefix.plot.sh
 done
