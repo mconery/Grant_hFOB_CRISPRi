a30b2f94945468c8e225dfaca31cf749d03a7dd7
Conery <conerym@reslnvhpc0264.research.chop.edu>
2024-01-27 02:06:51 -0500

    Updated thresholds for new version of analyses

--- Multi-Trait_Fine-Mapping/4_analyze_BMD_signals/extract_all_results.sh
+++ Multi-Trait_Fine-Mapping/4_analyze_BMD_signals/extract_all_results.sh
@@ -19,18 +19,19 @@ temp_script_dir=$finemap_dir/temp_script_files
 ###########################################################################################################################################################
 
 #Set parameters for testing
-assoc_threshes=(1 1e-6 5e-8)
-assoc_types=(gwas absolute_residual)
-purity_threshes=(0.95)
+assoc_threshes=(5e-8)
+assoc_type=gwas
+purity_threshes=(0.5)
+active_threshes=(0.5 0.95)
 
 #Change the directory to the temp directory
 cd $temp_script_dir
 
 #Loop over the loci and plot each
-for assoc_type in ${assoc_types[@]}; do 
+for active_thresh in ${active_threshes[@]}; do 
 	for assoc_thresh in ${assoc_threshes[@]}; do
 		for purity_thresh in ${purity_threshes[@]}; do 
-			echo "source activate cafeh; python $extract_script -p $cafeh_dir -o $out_dir -a $assoc_type -u $purity_thresh -m $assoc_thresh" > $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
+			echo "source activate cafeh; python $extract_script -p $cafeh_dir -o $out_dir -a $assoc_type -c $active_thresh -u $purity_thresh -m $assoc_thresh" > $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
 			sed -i '1i#!/bin/bash' $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
 			sbatch --mem=20G -t 2:00:00 --job-name $assoc_type.$purity_thresh.$assoc_thresh.extract $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
 
