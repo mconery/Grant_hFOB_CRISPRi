9efc2e28070f0a6f1f4adc9706d6f79694436a8e
Conery <conerym@reslnvhpc0264.research.chop.edu>
2024-01-29 16:40:07 -0500

    Updated for activity binarization and high-quality signal filtering

--- Multi-Trait_Fine-Mapping/4_analyze_BMD_signals/extract_all_results.sh
+++ Multi-Trait_Fine-Mapping/4_analyze_BMD_signals/extract_all_results.sh
@@ -16,13 +16,14 @@ cafeh_dir=$finemap_dir/cafeh_results
 out_dir=$finemap_dir/summary_files
 temp_script_dir=$finemap_dir/temp_script_files
 
-###########################################################################################################################################################
-
 #Set parameters for testing
 assoc_threshes=(5e-8)
 assoc_type=gwas
 purity_threshes=(0.5)
-active_threshes=(0.5 0.95)
+active_threshes=(0.95)
+filt_high_flags=(True False)
+
+###########################################################################################################################################################
 
 #Change the directory to the temp directory
 cd $temp_script_dir
@@ -30,11 +31,12 @@ cd $temp_script_dir
 #Loop over the loci and plot each
 for active_thresh in ${active_threshes[@]}; do 
 	for assoc_thresh in ${assoc_threshes[@]}; do
-		for purity_thresh in ${purity_threshes[@]}; do 
-			echo "source activate cafeh; python $extract_script -p $cafeh_dir -o $out_dir -a $assoc_type -c $active_thresh -u $purity_thresh -m $assoc_thresh" > $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
-			sed -i '1i#!/bin/bash' $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
-			sbatch --mem=20G -t 2:00:00 --job-name $assoc_type.$purity_thresh.$assoc_thresh.extract $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
-
+		for purity_thresh in ${purity_threshes[@]}; do
+			for flag in ${filt_high_flags[@]}; do
+				echo "source activate cafeh; python $extract_script -p $cafeh_dir -o $out_dir -a $assoc_type -c $active_thresh -u $purity_thresh -m $assoc_thresh" > $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
+				sed -i '1i#!/bin/bash' $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
+				sbatch --mem=20G -t 2:00:00 --job-name $assoc_type.$purity_thresh.$assoc_thresh.extract $temp_script_dir/$assoc_type.$purity_thresh.$assoc_thresh.extract.sh
+			done
 		done
 	done
 done
