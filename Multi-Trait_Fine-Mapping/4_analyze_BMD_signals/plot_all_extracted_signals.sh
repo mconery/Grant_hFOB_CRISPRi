86d893d59d0b9dc4c267c20550b942110f1270bc
Conery <conerym@reslnvhpc0264.research.chop.edu>
2024-01-30 10:59:18 -0500

    Added plotting for filtered signals

--- Multi-Trait_Fine-Mapping/4_analyze_BMD_signals/plot_all_extracted_signals.sh
+++ Multi-Trait_Fine-Mapping/4_analyze_BMD_signals/plot_all_extracted_signals.sh
@@ -14,6 +14,7 @@ plot_script=$script_dir/Multi-Trait_Fine-Mapping/4_analyze_BMD_signals/plot_BMD_
 finemap_dir=/mnt/isilon/sfgi/conerym/analyses/grant/multi-trait_fine-mapping/bmd_and_related
 cafeh_dir=$finemap_dir/cafeh_results
 plot_dir=$finemap_dir/bmd_signal_plots
+residual_filtered_plot_dir=$finemap_dir/residual-filtered_signal_plots
 temp_script_dir=$finemap_dir/temp_script_files
 
 #Set global variables
@@ -28,10 +29,15 @@ cd $temp_script_dir
 
 #Loop over the loci and plot each
 for file in $(ls $cafeh_dir); do 
-file_prefix=${file/%.pkl/}
-echo "source activate cafeh; python $plot_script -p $cafeh_dir/$file -o $plot_dir -u $min_purity -c $activity_thresh -m $p_thresh" > $temp_script_dir/$file_prefix.plot.sh
-sed -i '1i#!/bin/bash' $temp_script_dir/$file_prefix.plot.sh
-sbatch --mem=4G -t 2:00:00 --job-name $file_prefix $temp_script_dir/$file_prefix.plot.sh
+	file_prefix=${file/%.pkl/}
+	#Make signal plot
+	echo "source activate cafeh; python $plot_script -p $cafeh_dir/$file -o $plot_dir -u $min_purity -c $activity_thresh -m $p_thresh" > $temp_script_dir/$file_prefix.plot.sh
+	sed -i '1i#!/bin/bash' $temp_script_dir/$file_prefix.plot.sh
+	sbatch --mem=4G -t 2:00:00 --job-name $file_prefix $temp_script_dir/$file_prefix.plot.sh
+	#Make residual-filtered signal plot
+	echo "source activate cafeh; python $plot_script -p $cafeh_dir/$file -o $plot_dir -u $min_purity -c $activity_thresh -m $p_thresh -r True" > $temp_script_dir/$file_prefix.residual-filtered.plot.sh
+	sed -i '1i#!/bin/bash' $temp_script_dir/$file_prefix.residual-filtered.plot.sh
+	sbatch --mem=4G -t 2:00:00 --job-name $file_prefix.filtered $temp_script_dir/$file_prefix.residual-filtered.plot.sh
 done
 
 
