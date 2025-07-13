48853cd2bb60c1fb739712317866ba34edeff11a
mconery <mitchell.l.conery@gmail.com>
2025-07-13 00:34:54 -0500

    Added corner annotation

--- 7_Genetic_Correlations/make_rg_barplot.R
+++ 7_Genetic_Correlations/make_rg_barplot.R
@@ -151,7 +151,7 @@ p <- p + annotate(
   size = 3,
   color = "black"
 )
-ggsave(str_replace(no_second_bmd_plot_loc, pattern = ".jpeg", replacement = ".tiff"), 
+ggsave(str_replace(no_second_bmd_plot_loc, pattern = ".jpeg", replacement = ".tif"), 
        width = 6, height=2.2222, device = "tiff", units = "in", dpi=1000, plot = p)
 
 #Write table to file for use in supplement
